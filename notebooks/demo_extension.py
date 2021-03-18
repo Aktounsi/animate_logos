3801f9198e2e41c08b6b9e7327510591011ec7c5
Rebecca Armbruster <rebecca.armbruster@outlook.de>
2021-03-18 11:57:05 +0100

    Add animation prediction

--- notebooks/demo_extension.py
+++ notebooks/demo_extension.py
@@ -2,10 +2,11 @@ from src.models.train_animation_predictor import *
 import os
 import os, sys
 import pandas as pd
+from datetime import datetime
 
 os.chdir('..')
 
-from src.models.train_model_head import *
+from src.models.train_animation_predictor import *
 
 
 def main():
@@ -19,10 +20,14 @@ def main():
     emb_red = combined_embedding[combined_embedding['filename'].isin(easy_logos)].sort_values(['filename', 'animation_id'])
     emb_red.reset_index(drop=True, inplace=True)
 
-    top_model = train_model_head(path_level_dataset=emb_red, svg_level_dataset=svg_embedding, num_agents=5, top_parent_limit=3, generations=2, out_size=13)
+    print(emb_red.groupby(['animation_id']).size())
+
+    top_model = train_model_head(path_level_dataset=emb_red, svg_level_dataset=svg_embedding, num_agents=100, top_parent_limit=10, generations=5)
     torch.save(top_model, './models/best_model_head.pkl')
     torch.save(top_model.state_dict(), './models/best_model_head_state_dict.pth')
 
 
 if __name__ == '__main__':
+    start = datetime.now()
     main()
+    print(f'Total operation time: {datetime.now() - start}')
