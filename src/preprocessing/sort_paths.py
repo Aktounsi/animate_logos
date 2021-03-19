1a77d223863e3fa9b292e4ea934c8c8921afaa24
janschwedhelm <jan.schwedhelm@stads.de>
2021-03-19 14:51:06 +0100

    added argument that specifies after how many paths the list should be truncated

--- src/preprocessing/sort_paths.py
+++ src/preprocessing/sort_paths.py
@@ -3,7 +3,7 @@ from skimage.metrics import mean_squared_error
 import os
 
 
-def sort_by_relevance(path_selection_folder):
+def sort_by_relevance(path_selection_folder, nr_paths_trunc=8):
     nr_paths = len([name for name in os.listdir(path_selection_folder) if os.path.isfile(os.path.join(path_selection_folder, name))]) - 1
     relevance_scores = []
     img_origin = image.imread(os.path.join(path_selection_folder, "original.png"))
@@ -12,4 +12,4 @@ def sort_by_relevance(path_selection_folder):
         relevance_scores.append(mean_squared_error(img_origin, img_reduced))
     relevance_score_ordering = list(range(nr_paths))
     relevance_score_ordering.sort(key=lambda x: relevance_scores[x], reverse=True)
-    return relevance_score_ordering
+    return relevance_score_ordering[0:nr_paths_trunc]
