9c7d7e9c3b0b6fd37981bb24f8f777733ee244ac
janschwedhelm <jan.schwedhelm@stads.de>
2021-03-26 16:36:03 +0100

    Finished data set creating and saving

--- src/features/create_path_vector.py
+++ src/features/create_path_vector.py
@@ -22,7 +22,8 @@ def create_path_vectors(svg_folder, emb_file_path=None, fitted_pca=None, emb_len
             df = pickle.load(f)
     else:
         df = apply_embedding_model_to_svgs(data_folder="../../data/decomposed_svgs", save=False)
-    df.dropna(inplace=True)  # can be deleted if NaN rows do not exist anymore
+    df.dropna(inplace=True) # can be deleted if NaN rows do not exist anymore
+    df = df[df['filename'] != 'logo_126'].reset_index(drop=True) # drop logo_126 since there are problems with SVG metadata
 
     # train/test subsetting
     split = round(len(np.unique(df['filename'])) * train_frc)
@@ -45,23 +46,31 @@ def create_path_vectors(svg_folder, emb_file_path=None, fitted_pca=None, emb_len
         if avg_cols_svg:
             df = _get_svg_avg(df, avg_cols_svg, avg_diff)
 
-    # if size:
-    #     df['rel_width'] = df.apply(lambda row: _get_relative_size(svg_folder + '/' + row['filename'] + '.svg',
-    #                                                               row['animation_id'])[0], axis=1)
-    #     df['rel_height'] = df.apply(lambda row: _get_relative_size(svg_folder + '/' + row['filename'] + '.svg',
-    #                                                                row['animation_id'])[1], axis=1)
-    #
-    # if position:
-    #     df['rel_x_position'] = df.apply(lambda row: _get_relative_path_position(svg_folder + '/' + row['filename'] + '.svg',
-    #                                                                             row['animation_id'])[0], axis=1)
-    #     df['rel_y_position'] = df.apply(lambda row: _get_relative_path_position(svg_folder + '/' + row['filename'] + '.svg',
-    #                                                                             row['animation_id'])[1], axis=1)
+    if size:
+        df['rel_width'] = df.apply(lambda row: _get_relative_size(svg_folder + '/' + row['filename'] + '.svg',
+                                                                  row['animation_id'])[0], axis=1)
+        df['rel_height'] = df.apply(lambda row: _get_relative_size(svg_folder + '/' + row['filename'] + '.svg',
+                                                                   row['animation_id'])[1], axis=1)
+
+    if position:
+        df['rel_x_position'] = df.apply(lambda row: _get_relative_path_position(svg_folder + '/' + row['filename'] + '.svg',
+                                                                                row['animation_id'])[0], axis=1)
+        df['rel_y_position'] = df.apply(lambda row: _get_relative_path_position(svg_folder + '/' + row['filename'] + '.svg',
+                                                                                row['animation_id'])[1], axis=1)
 
     if nr_paths_svg:
-        df_meta = get_svg_meta_data(data_folder=svg_folder)
+        meta_df = get_svg_meta_data(data_folder=svg_folder)
+        df = df.merge(meta_df[['id','nb_groups']], how='left', left_on=['filename'], right_on=['id'])
+        df.drop(['id'], axis=1, inplace=True)
+        df = df.rename(columns={'nb_groups': 'nr_paths_svg'})
+
+    if nr_commands:
+        meta_df = get_svg_meta_data(data_folder=svg_folder)
+        df['nr_commands'] = df.apply(lambda row: meta_df[meta_df['id'] == row['filename']].reset_index()['len_groups'][0][int(row['animation_id'])],
+                                     axis=1)
 
     if train:
-        return df_meta, fitted_pca
+        return df, fitted_pca
     else:
         return df
 
@@ -150,15 +159,11 @@ def _get_svg_avg(df, columns, diff=True):
 
 
 if __name__ == '__main__':
-    # train_df, fitted_pca = create_path_vectors("../../data/svgs", emb_file_path="../../data/path_embedding.pkl",
-    #                                            train=True)
-    # train_df.to_csv('../../data/X_train_model1.csv')
-    # print('Train data created and saved.')
-    # test_df = create_path_vectors("../../data/svgs", emb_file_path="../../data/path_embedding.pkl",
-    #                               train=False, fitted_pca=fitted_pca)
-    # test_df.to_csv('../../data/X_test_model1.csv')
-    # print('Test data created and saved.')
-
-    df_meta, _ = create_path_vectors("../../data/svgs", emb_file_path="../../data/path_embedding.pkl",
-                                  train=True)
-    print(df_meta)
+    train_df, fitted_pca = create_path_vectors("../../data/svgs", emb_file_path="../../data/path_embedding.pkl",
+                                               train=True)
+    train_df.to_csv('../../data/X_train_model1.csv')
+    print('Train data created and saved.')
+    test_df = create_path_vectors("../../data/svgs", emb_file_path="../../data/path_embedding.pkl",
+                                  train=False, fitted_pca=fitted_pca)
+    test_df.to_csv('../../data/X_test_model1.csv')
+    print('Test data created and saved.')
