7ffdf408706bd1760de0af8baacd54a792339235
Sarah Sester <sarahsester@web.de>
2021-03-29 08:41:53 +0200

    Minor changes in animation creation.

--- src/models_output/transform_animation_predictor_output.py
+++ src/models_output/transform_animation_predictor_output.py
@@ -6,8 +6,9 @@ from src.features.get_style_attributes import get_style_attributes_path
 
 def transform_animation_predictor_output(file, animation_id, output):
     """ Function to translate the numeric model output to animation commands
+    Format of output: (translate scale rotate skew fill opacity duration begin from_1 from_2 from_3)
 
-    Example: transform_animation_predictor_output("data/svgs/logo_1.svg", 0, [0,0,1,0,0,0,0.28,0.71,0.45,0.28,0.71])
+    Example: transform_animation_predictor_output("data/svgs/logo_1.svg", 0, [0,0,1,0,0,0,0.28,0.71,0.45,0.12,0.71])
 
     Args:
         output (list): 11-dimensional list of numeric values of which first 6 determine the animation to be used and
@@ -22,15 +23,13 @@ def transform_animation_predictor_output(file, animation_id, output):
     x_midpoint, y_midpoint = get_midpoint_of_path_bbox(file, animation_id)
     fill_style = get_style_attributes_path(file, animation_id, "fill")
     stroke_style = get_style_attributes_path(file, animation_id, "stroke")
-    stroke_width_style = get_style_attributes_path(file, animation_id, "stroke_width")
     opacity_style = get_style_attributes_path(file, animation_id, "opacity")
-    stroke_opacity_style = get_style_attributes_path(file, animation_id, "stroke_opacity")
 
     if output[0] == 1:
         animation["type"] = "translate"
-        xcoord = (output[8] * 2 - 1) * width
-        ycoord = (output[9] * 2 - 1) * height
-        animation["from_"] = f"{str(xcoord)} {str(ycoord)}"
+        x = (output[8] * 2 - 1) * width
+        y = (output[9] * 2 - 1) * height
+        animation["from_"] = f"{str(x)} {str(y)}"
         animation["to"] = "0 0"
 
     elif output[1] == 1:
@@ -46,9 +45,10 @@ def transform_animation_predictor_output(file, animation_id, output):
     elif output[3] == 1:
         if output[8] > 0.5:
             animation["type"] = "skewX"
+            animation["from_"] = (output[9] * 2 - 1) * width/10  # between -20 and 20
         else:
             animation["type"] = "skewY"
-        animation["from_"] = str(int(output[9]*40) - 20)  # between -20 and 20
+            animation["from_"] = (output[9] * 2 - 1) * height/10  # between -20 and 20
         animation["to"] = 0
 
     elif output[4] == 1:
@@ -68,7 +68,7 @@ def transform_animation_predictor_output(file, animation_id, output):
         animation["from_"] = 0
         animation["to"] = opacity_style
 
-    animation["dur"] = output[6] * 4  # between 0 and 4
+    animation["dur"] = (output[6] * 2) + 2  # between 2 and 4
     animation["begin"] = output[7] * 4  # between 0 and 4
     animation["fill"] = "freeze"
 
