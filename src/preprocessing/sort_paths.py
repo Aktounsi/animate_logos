3bc18caae0c215d34a952d5b284a2e1d6f1fcb79
Rebecca Armbruster <rebecca.armbruster@outlook.de>
2021-03-30 00:54:34 +0200

    Functions for selecting paths and truncating svgs

--- src/preprocessing/sort_paths.py
+++ src/preprocessing/sort_paths.py
@@ -4,12 +4,16 @@ from os.path import isfile, join
 from xml.dom import minidom
 from pathlib import Path
 from matplotlib import image
+from datetime import datetime
+from shutil import copyfile
 from skimage.metrics import mean_squared_error
 from src.models.transform_model_output_to_animation_states import convert_svgs_in_folder
 
+dir_svgs = './data/svgs'
 dir_path_selection = './data/path_selection'
 dir_truncated_svgs = './data/truncated_svgs'
-
+dir_selected_paths = './data/selected_paths'
+dir_decomposed_svgs = './data/decomposed_svgs'
 
 def get_elements(doc):
     return doc.getElementsByTagName('path') + doc.getElementsByTagName('circle') + doc.getElementsByTagName(
@@ -20,7 +24,7 @@ def get_elements(doc):
 
 def delete_paths(logo):
     Path(f'{dir_path_selection}/{logo}').mkdir(parents=True, exist_ok=True)
-    doc = minidom.parse(f'./data/svgs/{logo}.svg')
+    doc = minidom.parse(f'{dir_svgs}/{logo}.svg')
     nb_original_elements = len(get_elements(doc))
     with open(f'{dir_path_selection}/{logo}/original.svg', 'wb') as file:
         file.write(doc.toprettyxml(encoding='iso-8859-1'))
@@ -38,15 +42,23 @@ def delete_paths(logo):
 
 
 def sort_by_relevance(path_selection_folder, not_embedded_paths, nr_paths_trunc=8):
+    # Todo: Include not_embedded_paths = None
     nr_paths = len([name for name in os.listdir(path_selection_folder) if os.path.isfile(os.path.join(path_selection_folder, name))]) - 1
     relevance_scores = []
+    missed_scores = []
     img_origin = image.imread(os.path.join(path_selection_folder, "original.png"))
     logo = path_selection_folder.split('/')[-1]
     for i in range(nr_paths):
         img_reduced = image.imread(os.path.join(path_selection_folder, "without_id_{}.png".format(i)))
         try:
             decomposed_id = f'{logo}_{i}'
-            mse = mean_squared_error(img_origin, img_reduced) if decomposed_id not in not_embedded_paths else -1
+            if decomposed_id in not_embedded_paths:
+                missed_mse = mean_squared_error(img_origin, img_reduced)
+                missed_scores.append(missed_mse)
+                print(f'No path embedding for animation ID {i} in logo {logo}, actual MSE would be: {missed_mse}')
+                mse = -1
+            else:
+                mse = mean_squared_error(img_origin, img_reduced)
         except ValueError as e:
             print(f'Could not calculate MSE for animation id {i} in logo {path_selection_folder} - Error message: {e}')
             mse = -1
@@ -54,17 +66,40 @@ def sort_by_relevance(path_selection_folder, not_embedded_paths, nr_paths_trunc=
     relevance_score_ordering = list(range(nr_paths))
     relevance_score_ordering.sort(key=lambda x: relevance_scores[x], reverse=True)
     relevance_score_ordering = relevance_score_ordering[0:nr_paths_trunc]
+    missed_relevant_scores = [score for score in missed_scores if score >= relevance_scores[relevance_score_ordering[-1]]]
+    if len(missed_relevant_scores) > 0:
+        print(f'>>> Number of missed relevant paths due to embedding: {len(missed_relevant_scores)}')
     relevance_score_ordering = [id_ if relevance_scores[id_] != -1 else -1 for id_ in relevance_score_ordering]
-    return relevance_score_ordering[0:nr_paths_trunc], relevance_scores
+    return relevance_score_ordering, relevance_scores, missed_relevant_scores
 
 
-def truncate_svgs(svgs_folder, nr_paths_trunc=8):
+def select_paths(svgs_folder, not_embedded_paths):
+    Path(dir_selected_paths).mkdir(parents=True, exist_ok=True)
+    logos = [f[:-4] for f in listdir(svgs_folder) if isfile(join(svgs_folder, f))]
+    start = datetime.now()
+    missed_scores = list()
+    for i, logo in enumerate(logos):
+        if i % 20 == 0:
+            print(f'Current logo: {i}/{len(logos)}')
+        sorted_ids, sorted_mses, missed_relevant_scores = sort_by_relevance(f'{dir_path_selection}/{logo}', not_embedded_paths)
+        missed_scores.append(len(missed_relevant_scores))
+        copyfile(f'{svgs_folder}/{logo}.svg', f'{dir_selected_paths}/{logo}_path_full.svg')
+        for j, id_ in enumerate(sorted_ids):
+            copyfile(f'{dir_decomposed_svgs}/{logo}_{id_}.svg', f'{dir_selected_paths}/{logo}_path_{j}.svg')
+    print(f'Time: {datetime.now() - start}')
+    try:
+        print(f'Total number of missed paths: {sum(missed_scores)}')
+    except:
+        return missed_scores
+
+
+def truncate_svgs(svgs_folder, not_embedded_paths, nr_paths_trunc=8):
     Path(dir_truncated_svgs).mkdir(parents=True, exist_ok=True)
     logos = [f[:-4] for f in listdir(svgs_folder) if isfile(join(svgs_folder, f))]
     for i, logo in enumerate(logos):
         if i % 20 == 0:
             print(f'Current logo {i}/{len(logos)}: {logo}')
-        sorted_ids, _ = sort_by_relevance(f'{dir_path_selection}/{logo}', nr_paths_trunc)
+        sorted_ids, _, _ = sort_by_relevance(f'{dir_path_selection}/{logo}', not_embedded_paths, nr_paths_trunc)
         doc = minidom.parse(f'{svgs_folder}/{logo}.svg')
         original_elements = get_elements(doc)
         nb_original_elements = len(original_elements)
