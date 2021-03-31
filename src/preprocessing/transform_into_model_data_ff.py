557e4fbfb53a0e673666d9c6881a0e8ccef575d0
janschwedhelm <jan.schwedhelm@stads.de>
2021-03-31 12:49:10 +0200

    Fixed encode_classes function

--- src/preprocessing/transform_into_model_data_ff.py
+++ src/preprocessing/transform_into_model_data_ff.py
@@ -1,23 +1,17 @@
 import numpy as np
 
+
 def encode_classes(targets: np.ndarray):
-    '''
-
-    Args:
-        targets: np.array of
-
-    Returns:
-
-    '''
+    targets_encoded = np.zeros(shape=[targets.shape[0], 4], dtype=np.int_)
     for i in range(targets.shape[0]):
-        if targets[0][i] == 0:
-            targets[0][i] = np.array([0, 0, 0, 0])
-        elif targets[0][i] == 1:
-            targets[0][i] = np.array([1, 0, 0, 0])
-        elif targets[0][i] == 2:
-            targets[0][i] = np.array([1, 1, 0, 0])
-        elif targets[0][i] == 3:
-            targets[0][i] = np.array([1, 1, 1, 0])
+        if targets[i] == 0:
+            targets_encoded[i] = np.array([0, 0, 0, 0])
+        elif targets[i] == 1:
+            targets_encoded[i] = np.array([1, 0, 0, 0])
+        elif targets[i] == 2:
+            targets_encoded[i] = np.array([1, 1, 0, 0])
+        elif targets[i] == 3:
+            targets_encoded[i] = np.array([1, 1, 1, 0])
         else:
-            targets[0][i] = np.array([1, 1, 1, 1])
-    return targets
+            targets_encoded[i] = np.array([1, 1, 1, 1])
+    return targets_encoded
