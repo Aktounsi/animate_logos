aca2cf84a02d716e201e589839754ad7d022cc7f
Sarah Sester <sarahsester@web.de>
2021-04-02 11:29:45 +0200

    Filtered and renamed new logos.

--- src/data/rename_logos.py
+++ src/data/rename_logos.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 
 
-def rename_logos(old_folder, new_folder="data/svgs"):
+def rename_logos(old_folder, new_folder="data/svgs", start_with=0):
     """ Function to rename all logos in a folder.
 
     Example: rename_logos("data/svgs_expanded", "data/svgs")
@@ -11,13 +11,13 @@ def rename_logos(old_folder, new_folder="data/svgs"):
     Args:
         old_folder (string): The path of the folder with all SVGs.
         new_folder (string): The path of the folder with the renamed SVGs.
-
+        start_with (int): First value of renamed logos.
     """
     Path(new_folder).mkdir(parents=True, exist_ok=True)
     for i, file in enumerate(os.listdir(old_folder)):
         if file.endswith('.svg'):
             doc = minidom.parse(old_folder + '/' + file)
-            textfile = open(new_folder + '/logo_' + str(i) + '.svg', 'wb')
+            textfile = open(new_folder + '/logo_' + str(start_with + i) + '.svg', 'wb')
             textfile.write(doc.toprettyxml(encoding="iso-8859-1"))  # needed to handle "Umlaute"
             textfile.close()
             doc.unlink()
