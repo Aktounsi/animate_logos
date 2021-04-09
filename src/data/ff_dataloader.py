a41684b1a7767f81d468ef7341ad4b555a97c3d8
janschwedhelm <jan.schwedhelm@stads.de>
2021-04-09 13:04:48 +0200

    Adapted DatasetFF to new input structure for surrogate model

--- src/data/ff_dataloader.py
+++ src/data/ff_dataloader.py
@@ -2,10 +2,6 @@ import torch
 import pandas as pd
 import numpy as np
 
-FEATURE_ID_TO_SCALE = [6,7,8,9,10,17,18,19,20,21,28,29,30,31,32,39,40,41,42,43,50,51,52,53,54,61,62,63,64,65,
-                       72,73,74,74,76,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,
-                       108,109,110,111,112,113,114,115,116,117,118,119]
-
 
 class DatasetFF(torch.utils.data.Dataset):
 
@@ -27,7 +23,7 @@ class DatasetFF(torch.utils.data.Dataset):
 
     def scale(self, fitted_scaler):
         sc = fitted_scaler
-        self.X[:, FEATURE_ID_TO_SCALE] = torch.from_numpy(sc.transform(self.X[:, FEATURE_ID_TO_SCALE]).astype(np.float32))
+        self.X[:, 6:] = torch.from_numpy(sc.transform(self.X[:, 6:]).astype(np.float32))
 
     def __len__(self):
         # Denotes the total number of samples
