6c27b3508eeeb03c5005e69c1b152a86a4e975f6
janschwedhelm <jan.schwedhelm@stads.de>
2021-04-20 10:53:18 +0200

    Adapted blackbox_sm_fnn.py

--- src/models/blackbox_sm_fnn.py
+++ src/models/blackbox_sm_fnn.py
@@ -1,22 +1,29 @@
 from entmoot.benchmarks import BenchmarkFunction
+from entmoot.space.space import Integer
+from src.models.ordinal_classifier_fnn import *
+import torch
 
 
 class SurrogateModelFNN(BenchmarkFunction):
 
     def __init__(self, func_config={}):
-        from entmoot.space.space import Integer
-        self.cat_dims = [
-            Integer(low=0, high=1) for _ in range(2)
-        ]
+        self.an_statement_dims = [Integer(low=0, high=1) for _ in range(6)]
+        self.an_parameters_dims = [(0.0, 1.0) for _ in range(6)]
         self.name = 'surrogate_model_fnn'
-        self.func_config = func_config
-        self.y_opt = 0.0
 
-    def get_bounds(self, n_dim=2):
-        return self.cat_dims + [(0.0, 1.0) for _ in range(n_dim)]
+        # Load surrogate model
+        self.func = OrdinalClassifierFNN(num_classes=5, layer_sizes=[38, 28])
+        self.func.load_state_dict(torch.load("models/sm_fnn.pth"))
+        self.func.eval()
 
-    def get_X_opt(self, n_dim=2):
+    def get_bounds(self, n_dim=28):
+        return self.an_statement_dims + self.an_parameters_dims + [(-10.0, 10.0) for _ in range(n_dim)]
+
+    def get_X_opt(self):
         pass
 
-    def _eval_point(self, X):
-        return X[0] + X[1] + X[2] + X[3]
+    def _eval_point(self, x):
+        x = torch.tensor(x)
+        logits = self.func(x)
+        output = decode_classes(torch.sigmoid(logits))[0][0]
+        return output
