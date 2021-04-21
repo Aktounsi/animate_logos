0458c7318fd32b3391ebcb1a3bd2c519df9297aa
janschwedhelm <jan.schwedhelm@stads.de>
2021-04-21 14:00:21 +0200

    Adapted test_bo_optimization.py

--- src/models/test_bo_optimization.py
+++ src/models/test_bo_optimization.py
@@ -8,15 +8,19 @@ from entmoot.optimizer.entmoot_minimize import entmoot_minimize
 # Load data
 initial_data = pd.read_csv('../../data/surrogate_model/sm_train_data_scaled.csv')
 
-X_train = initial_data.iloc[:100,:-4]
-X_train.replace(to_replace=-1, value=0, inplace=True
-               )
-y_train = initial_data.iloc[:100,-4:]
+X_train = initial_data.iloc[:200,:-4]
+X_train.replace(to_replace=-1, value=0, inplace=True)
+y_train = initial_data.iloc[:200,-4:]
 
 y_train = pd.Series(decode_classes(y_train.to_numpy()).flatten()) * -1
 
 X_train = X_train.values.tolist()
 y_train = y_train.values.tolist()
+
+path_vector = [0.4813, -1.0060, -0.5242,  1.3296,
+         0.2993, -0.3018, -1.6347, -0.4806, -0.6526, -0.7260, -1.2620, -0.7265,
+         0.3742,  0.2481,  0.4610,  0.6932, -1.5797, -1.1766, -0.1511,  1.6870,
+        -0.3128,  0.0441,  0.8950,  0.9627,  0.0286, -0.5782]
 
 # Load surrogate model
 func = SurrogateModelFNN()
@@ -66,32 +70,70 @@ rel_x_position_to_animations = core_model._cont_var_dict[35]
 rel_y_position_to_animations = core_model._cont_var_dict[36]
 nr_paths_svg = core_model._cont_var_dict[37]
 
-core_model.addConstr(emb_0 == 1)
-core_model.addConstr(emb_1 == 1)
-core_model.addConstr(emb_2 == 1)
-core_model.addConstr(emb_3 == 1)
-core_model.addConstr(emb_4 == 1)
-core_model.addConstr(emb_5 == 1)
-core_model.addConstr(emb_6 == 1)
-core_model.addConstr(emb_7 == 1)
-core_model.addConstr(emb_8 == 1)
-core_model.addConstr(emb_9 == 1)
-core_model.addConstr(fill_r == 1)
-core_model.addConstr(fill_g == 1)
-core_model.addConstr(fill_b == 1)
-core_model.addConstr(svg_fill_r == 1)
-core_model.addConstr(svg_fill_g == 1)
-core_model.addConstr(svg_fill_b == 1)
-core_model.addConstr(diff_fill_r == 1)
-core_model.addConstr(diff_fill_g == 1)
-core_model.addConstr(diff_fill_b == 1)
-core_model.addConstr(rel_height == 1)
-core_model.addConstr(rel_width == 1)
-core_model.addConstr(rel_x_position == 1)
-core_model.addConstr(rel_y_position == 1)
-core_model.addConstr(rel_x_position_to_animations == 1)
-core_model.addConstr(rel_y_position_to_animations == 1)
-core_model.addConstr(nr_paths_svg == 1)
+core_model.addConstr(an_vec_0 + an_vec_1 + an_vec_2 + an_vec_3 + an_vec_4 + an_vec_5 == 1)
+
+core_model.addConstr(an_vec_8 == (1-an_vec_0)*an_vec_8)
+core_model.addConstr(an_vec_9 == (1-an_vec_0)*an_vec_9)
+core_model.addConstr(an_vec_10 == (1-an_vec_0)*an_vec_10)
+core_model.addConstr(an_vec_11 == (1-an_vec_0)*an_vec_11)
+
+core_model.addConstr(an_vec_6 == (1 - an_vec_1) * an_vec_6)
+core_model.addConstr(an_vec_7 == (1 - an_vec_1) * an_vec_7)
+core_model.addConstr(an_vec_9 == (1 - an_vec_1) * an_vec_9)
+core_model.addConstr(an_vec_10 == (1 - an_vec_1) * an_vec_10)
+core_model.addConstr(an_vec_11 == (1 - an_vec_1) * an_vec_11)
+
+core_model.addConstr(an_vec_6 == (1 - an_vec_2) * an_vec_6)
+core_model.addConstr(an_vec_7 == (1 - an_vec_2) * an_vec_7)
+core_model.addConstr(an_vec_8 == (1 - an_vec_2) * an_vec_8)
+core_model.addConstr(an_vec_10 == (1 - an_vec_2) * an_vec_10)
+core_model.addConstr(an_vec_11 == (1 - an_vec_2) * an_vec_11)
+
+core_model.addConstr(an_vec_6 == (1 - an_vec_3) * an_vec_6)
+core_model.addConstr(an_vec_7 == (1 - an_vec_3) * an_vec_7)
+core_model.addConstr(an_vec_8 == (1 - an_vec_3) * an_vec_8)
+core_model.addConstr(an_vec_9 == (1 - an_vec_3) * an_vec_9)
+
+core_model.addConstr(an_vec_6 == (1 - an_vec_4) * an_vec_6)
+core_model.addConstr(an_vec_7 == (1 - an_vec_4) * an_vec_7)
+core_model.addConstr(an_vec_8 == (1 - an_vec_4) * an_vec_8)
+core_model.addConstr(an_vec_9 == (1 - an_vec_4) * an_vec_9)
+core_model.addConstr(an_vec_10 == (1 - an_vec_4) * an_vec_10)
+core_model.addConstr(an_vec_11 == (1 - an_vec_4) * an_vec_11)
+
+core_model.addConstr(an_vec_6 == (1 - an_vec_5) * an_vec_6)
+core_model.addConstr(an_vec_7 == (1 - an_vec_5) * an_vec_7)
+core_model.addConstr(an_vec_8 == (1 - an_vec_5) * an_vec_8)
+core_model.addConstr(an_vec_9 == (1 - an_vec_5) * an_vec_9)
+core_model.addConstr(an_vec_10 == (1 - an_vec_5) * an_vec_10)
+core_model.addConstr(an_vec_11 == (1 - an_vec_5) * an_vec_11)
+
+core_model.addConstr(emb_0 == path_vector[0])
+core_model.addConstr(emb_1 == path_vector[1])
+core_model.addConstr(emb_2 == path_vector[2])
+core_model.addConstr(emb_3 == path_vector[3])
+core_model.addConstr(emb_4 == path_vector[4])
+core_model.addConstr(emb_5 == path_vector[5])
+core_model.addConstr(emb_6 == path_vector[6])
+core_model.addConstr(emb_7 == path_vector[7])
+core_model.addConstr(emb_8 == path_vector[8])
+core_model.addConstr(emb_9 == path_vector[9])
+core_model.addConstr(fill_r == path_vector[10])
+core_model.addConstr(fill_g == path_vector[11])
+core_model.addConstr(fill_b == path_vector[12])
+core_model.addConstr(svg_fill_r == path_vector[13])
+core_model.addConstr(svg_fill_g == path_vector[14])
+core_model.addConstr(svg_fill_b == path_vector[15])
+core_model.addConstr(diff_fill_r == path_vector[16])
+core_model.addConstr(diff_fill_g == path_vector[17])
+core_model.addConstr(diff_fill_b == path_vector[18])
+core_model.addConstr(rel_height == path_vector[19])
+core_model.addConstr(rel_width == path_vector[20])
+core_model.addConstr(rel_x_position == path_vector[21])
+core_model.addConstr(rel_y_position == path_vector[22])
+core_model.addConstr(rel_x_position_to_animations == path_vector[23])
+core_model.addConstr(rel_y_position_to_animations == path_vector[24])
+core_model.addConstr(nr_paths_svg == path_vector[25])
 
 core_model.update()
 
@@ -99,7 +141,7 @@ core_model.update()
 res = entmoot_minimize(
     func,
     func.get_bounds(),
-    n_calls=1,
+    n_calls=3,
     base_estimator="GBRT",
     std_estimator="L1DDP",
     n_initial_points=0,
@@ -121,4 +163,4 @@ res = entmoot_minimize(
 )
 
 print(res['x_iters'][-1])
-print(res['func_vals'][-1])
+
